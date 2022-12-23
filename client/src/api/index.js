aa8fc8bf6c38277b34821ae864479d096577ca04
Namasivaayam-L <namasivaayam007vijay@gmail.com>
2022-12-23 09:44:59 +0530

    Changed the Base API Link

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: 'https://stack-overflow-clone-namsivaayam-l.onrender.com:10000' })
+const API = axios.create({ baseURL: 'https://stack-overflow-clone-namsivaayam-l.onrender.com' })
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('Profile')) {
