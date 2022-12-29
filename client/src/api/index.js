9a0cd5e60cc6fa88b9acbdb88bdd6f9b3d6bb019
Namasivaayam L <namasivaayam007vijay@gmail.com>
2022-12-29 23:00:10 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
-// const API = axios.create({ baseURL: 'https://stack-overflow-clone-namsivaayam-l.onrender.com' })
-const API = axios.create({ baseURL: 'http://localhost:5000' })
+const API = axios.create({ baseURL: process.env.REACT_APP_NODE_JS })
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('Profile')) {
