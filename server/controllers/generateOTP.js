57ee8aeaf87e5e6a0b3f626b517fa90e5f628245
Namasivaayam L <namasivaayam007vijay@gmail.com>
2022-12-30 08:02:35 +0530

    Update generateOTP.js

--- server/controllers/generateOTP.js
+++ server/controllers/generateOTP.js
@@ -4,44 +4,52 @@ import User from '../models/auth'
 import Mailer from '../utilities/mailer'
 
 export const generateOTP = async (req, res) => {
-    var user = await User.findOne({email: req.body.email})
-    if (user) {
-        console.log('server ctrl genOTP genOTP');
-        if(!await OTP.findOne({email: req.body.email})){
-            var otp = new OTP({
-            email: req.body.email,
-            otp: Math.floor((Math.random()*10000)+1),
-            expiresIn: new Date().getTime() + 300*1000
-        })
-            await otp.save()
-        } else {
-            var otp = await OTP.findOneAndUpdate({email: req.body.email},{
-                otp: Math.floor((Math.random()*10000)+1),
-                expiresIn: new Date().getTime() + 300*1000
-            },
-            {returnDocument:'after'})
-        }
-        if (Mailer(otp)){
-            return res.status(200).send({message:'Mail Sent Successfully'})
-        }else {
-            return res.status(500).send({message:'Sending Mail failed'})
-        }
-    } else {
-        return res.status(500).send({message:'User Not Found, Please register'})
-    }
+    try {
+	    var user = await User.findOne({email: req.body.email})
+	    if (user) {
+	        console.log('server ctrl genOTP genOTP');
+	        if(!await OTP.findOne({email: req.body.email})){
+	            var otp = new OTP({
+	            email: req.body.email,
+	            otp: Math.floor((Math.random()*10000)+1),
+	            expiresIn: new Date().getTime() + 300*1000
+	            })
+	            await otp.save()
+	        } else {
+	            var otp = await OTP.findOneAndUpdate({email: req.body.email},{
+	                otp: Math.floor((Math.random()*10000)+1),
+	                expiresIn: new Date().getTime() + 300*1000
+	            },
+	            {returnDocument:'after'})
+	        }
+	        const otpStatus = Mailer(otp)
+	        if (otpStatus){
+	            return res.status(200).json({message:'Mail Sent Successfully'})
+	        }else {
+	            return res.status(500).json({message:'Sending Mail failed'})
+	        }
+	    } else {
+	        return res.status(500).json({message:'User Not Found, Please register'})
+	    }
+    } catch (error) {
+        return res.status(500).json({message:error})
+	}
 }
 
 export const verifyOTP = async (req, res) => {
-    console.log(req.body);
-    var user = await OTP.findOne({email: req.body.email})
-    if (user) {
-        if(req.body.recvOTP === user.otp){
-            return res.status(200).send({message:true})
-        }
-        else{
-            return res.status(500).send({message:false})
-        }
-    }else {
-        return res.status(500).send({message:'Retry'})
+    try {
+	var user = await OTP.findOne({email: req.body.email})
+	    if (user) {
+	        if(req.body.recvOTP === user.otp){
+	            return res.status(200).json({message:true})
+	        }
+	        else{
+	            return res.status(500).json({message:false})
+	        }
+	    }else {
+	        return res.status(500).json({message:'Retry'})
+	    }
+    } catch (error) {
+    return res.status(500).json({message:'Retry'+error})
     }
 }
