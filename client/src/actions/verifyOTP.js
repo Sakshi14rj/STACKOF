1fdb15b190d31e5fe53d1aeb2926833eab9b8cf8
Namasivaayam L <namasivaayam007vijay@gmail.com>
2022-12-30 07:46:33 +0530

    Update verifyOTP.js

--- client/src/actions/verifyOTP.js
+++ client/src/actions/verifyOTP.js
@@ -2,7 +2,6 @@ import * as api from '../api'
 
 export const generateOTP = async(email) =>{
     try {
-        console.log(email);
         const { data } = await api.generateOTP(email)
         return data
     } catch (error) {
@@ -12,8 +11,8 @@ export const generateOTP = async(email) =>{
 
 export const verifyOTP = async (email,recvOTP) => {
     try {
-        console.log(email);
-        return await (await api.verifyOTP(email,recvOTP)).data
+        const { data } = await api.verifyOTP(email,recvOTP)
+        return data
     } catch (error) {
         console.log('src actions verifyOTP verifyOTP',error)
     }
